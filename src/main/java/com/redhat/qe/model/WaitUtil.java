54d31afc48c9cf19ec80773c1a8d948af5104d92
Dustin Tsang <dtsang@redhat.com>
2013-04-08 11:23:05 -0400

    generic waiter added

--- src/main/java/com/redhat/qe/model/WaitUtil.java
+++ src/main/java/com/redhat/qe/model/WaitUtil.java
@@ -2,6 +2,9 @@ package com.redhat.qe.model;
 
 import com.redhat.qe.repository.HostRepository;
 
+import dstywho.functional.Predicate;
+import dstywho.timeout.Duration;
+
 public class WaitUtil {
 	
 	public static boolean waitForHostStatus(HostRepository repo, Host host, String status, int numAttempts){
@@ -9,6 +12,16 @@ public class WaitUtil {
 		while(attempt < numAttempts){
 			try{Thread.sleep(1000);}catch(Exception e){throw new RuntimeException(e);}
 			if(repo.show(host).getState().equals(status))
+				return true;
+			numAttempts++;
+		}
+		return false;
+	}
+	
+	public static boolean waitUntil(Predicate condition, int numAttempts){
+		for(int _ : new int[numAttempts]){
+			Duration.ONE_SECOND.sleep();
+			if(condition.call())
 				return true;
 		}
 		return false;
