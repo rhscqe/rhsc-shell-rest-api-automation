7e8dbe7005fb93c2ff37ebceaf92c81ab24effb8
Dustin Tsang <dtsang@redhat.com>
2013-04-09 16:38:28 -0400

    dry up waitutil and more logging

--- src/main/java/com/redhat/qe/model/Model.java
+++ src/main/java/com/redhat/qe/model/Model.java
@@ -1,11 +1,16 @@
 package com.redhat.qe.model;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Model {
 	
 	public static <T> T fromJson(String json,Class<T> clazz){
 		return new Gson().fromJson(json,clazz);
+	}
+	public String toJson(){
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		return gson.toJson(this);
 	}
 
 }
--- src/main/java/com/redhat/qe/model/WaitUtil.java
+++ src/main/java/com/redhat/qe/model/WaitUtil.java
@@ -1,24 +1,37 @@
 package com.redhat.qe.model;
 
+
+import org.apache.log4j.Logger;
+
 import com.redhat.qe.repository.HostRepository;
 
 import dstywho.functional.Predicate;
 import dstywho.timeout.Duration;
 
 public class WaitUtil {
+	private static Logger LOG = Logger.getLogger(WaitUtil.class);
 
-	public static boolean waitForHostStatus(HostRepository repo, Host host, String status, int numAttempts) {
-		for (int _ : new int[numAttempts]) {
-			Duration.ONE_SECOND.sleep();
-			if (repo.show(host).getState().equals(status))
-				return true;
+	public static boolean waitForHostStatus(final HostRepository repo, final Host host, final String status, int numAttempts) {
+		long starttime = System.currentTimeMillis();
+		boolean result = waitUntil(new Predicate() {
+			
+			@Override
+			public Boolean act() {
+				return repo.show(host).getState().equals(status);
+			}
+		}, numAttempts);
+		if(result){
+			LOG.debug(String.format("succeeded in waiting for host status %s. Number of Attmepts: %s. Elapsed Time(ms):", status, numAttempts, System.currentTimeMillis() - starttime));
+		}else{
+			LOG.debug(String.format("failed in waiting for host status %s.host status Number of Attmepts: %s. Elapsed Time(ms):", status, numAttempts, System.currentTimeMillis() - starttime));
 		}
-		return false;
+		return result;
+			
 	}
 
 	public static boolean waitUntil(Predicate condition, int numAttempts) {
 		for (int _ : new int[numAttempts]) {
-			Duration.ONE_SECOND.sleep();
+			Duration.TEN_SECONDS.sleep();
 			if (condition.call())
 				return true;
 		}
