e3631f32651c965b422654cc0cb8befd084af486
Dustin Tsang <dtsang@redhat.com>
2013-05-15 12:12:02 -0400

    250541 automated--ping test

---
+++ src/test/java/com/redhat/qe/test/PingTest.java
@@ -0,0 +1,18 @@
+package com.redhat.qe.test;
+
+
+import org.junit.Test;
+
+import com.redhat.qe.annoations.Tcms;
+
+public class PingTest extends TestBase {
+
+
+	@Tcms({"250541"})
+	@Test
+	public void pingTest() {
+		shell.send("ping").expect("(?i)success: oVirt manager could be reached OK.");
+	}
+
+
+}
