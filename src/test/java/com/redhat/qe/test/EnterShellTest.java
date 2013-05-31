0679a9f8913a8b41993cd5a0f8d141dc135dba99
Dustin Tsang <dtsang@redhat.com>
2013-05-31 12:29:45 -0400

    update enter shell test to check for ovirt or rhsc welcome message

--- src/test/java/com/redhat/qe/test/EnterShellTest.java
+++ src/test/java/com/redhat/qe/test/EnterShellTest.java
@@ -8,7 +8,7 @@ public class EnterShellTest extends ShellSessionTestBase{
 	@Test
 	@Tcms("250459")
 	public void test(){
-		shell.send("rhsc-shell").expect("Welcome to RHSC shell(.|\n|\r)*disconnected");
+		shell.send("rhsc-shell || ovirt-shell ").expect("Welcome to (RHSC|OVIRT) shell");
 	}
 
 }
