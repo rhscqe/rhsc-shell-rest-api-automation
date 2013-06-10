55eb19e8d8a76445962a3a319f140c0c5a4fbdc6
Dustin Tsang <dtsang@redhat.com>
2013-06-10 09:35:45 -0400

    ovirt shell expected prompt includes the connection status now

--- src/main/java/com/redhat/qe/ssh/RhscShellReadInput.java
+++ src/main/java/com/redhat/qe/ssh/RhscShellReadInput.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 
 public class RhscShellReadInput extends ReadInput implements Callable<Response>{
-	public static final Pattern RHSC_PROMPT = Pattern.compile("(\\[RHSC\\sshell\\s\\(\\w+\\)\\]#\\s)|OVIRT shell");
+	public static final Pattern RHSC_PROMPT = Pattern.compile("(\\[RHSC\\sshell\\s\\(\\w+\\)\\]#\\s)|(OVIRT shell\\s\\(\\w+\\)\\]#)");
 
 	public RhscShellReadInput(InputStream inputStream, Duration timeout) {
 		super(inputStream, timeout);
