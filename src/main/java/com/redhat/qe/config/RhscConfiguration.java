5ce97a27f0ace7b90c2461d30def97ea8ce2ed24
Dustin Tsang <dtsang@redhat.com>
2013-06-28 14:25:23 -0400

    use environment variable to select a different  configuration file

--- src/main/java/com/redhat/qe/config/RhscConfiguration.java
+++ src/main/java/com/redhat/qe/config/RhscConfiguration.java
@@ -9,14 +9,28 @@ import dstywho.FileUtil;
 
 public class RhscConfiguration {
 
+	private static final String DEFAULT_CONFIG = "src/test/resources/rhsc-config.xml";
 	private static Configuration INSTANCE;
 
 	public static synchronized Configuration getConfiguration(){
 		try {
-			return INSTANCE == null ? Configuration.fromXml(FileUtil.fileToString(new File("src/test/resources/rhsc-config.xml"))) : INSTANCE;
+			return INSTANCE == null ? getConfigFromXml() : INSTANCE;
 		} catch (IOException e) {
 			throw new UnableToOpenConfigurationFileException(e);
 		}
+	}
+	
+	public static String getXmlFile(){
+		String result = System.getenv("RHSC_SHELL_TEST_CONFIG_FILE");
+		if (result == null){
+			return DEFAULT_CONFIG;
+		}else{
+			return result;
+		}
+	}
+
+	private static Configuration getConfigFromXml() throws IOException {
+		return Configuration.fromXml(FileUtil.fileToString(new File(getXmlFile())));
 	}
 	 
 }
