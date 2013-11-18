df4c810a15b11c321e02d6674ebfb5bf9023025c
Dustin Tsang <dtsang@redhat.com>
2013-11-18 07:09:28 -0800

    311350 start rebal on nonexistent volume

---
+++ src/test/java/com/redhat/qe/test/rest/rebalance/StartRebalanceNonExistentVolumeTest.java
@@ -0,0 +1,24 @@
+package com.redhat.qe.test.rest.rebalance;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.redhat.qe.annoations.Tcms;
+import com.redhat.qe.factories.VolumeFactory;
+import com.redhat.qe.model.Volume;
+import com.redhat.qe.repository.rest.ResponseWrapper;
+import com.redhat.qe.test.rest.TwoHostClusterTestBase;
+
+public class StartRebalanceNonExistentVolumeTest extends TwoHostClusterTestBase{
+
+	@Test
+	@Tcms("311350")
+	public void test(){
+		Volume volume = VolumeFactory.distributedUneven("startrebalancetest", getHost1(), getHost2());
+		volume.setId("thisisafakeid");
+		ResponseWrapper resp = getVolumeRepository(getHost1().getCluster())._rebalance(volume);
+		Assert.assertEquals(404, resp.getCode());
+	}
+
+}
