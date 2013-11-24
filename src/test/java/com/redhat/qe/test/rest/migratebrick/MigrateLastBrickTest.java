10d27190946028823e76334c088f75c13a32b6ce
Dustin Tsang <dtsang@redhat.com>
2013-11-24 08:48:48 -0800

    318705 valiation for removal of last brick

---
+++ src/test/java/com/redhat/qe/test/rest/migratebrick/MigrateLastBrickTest.java
@@ -0,0 +1,60 @@
+package com.redhat.qe.test.rest.migratebrick;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.base.Function;
+import com.redhat.qe.annoations.Tcms;
+import com.redhat.qe.config.RhscConfiguration;
+import com.redhat.qe.factories.VolumeFactory;
+import com.redhat.qe.helpers.Asserts;
+import com.redhat.qe.helpers.repository.StepsRepositoryHelper;
+import com.redhat.qe.model.Brick;
+import com.redhat.qe.model.Job;
+import com.redhat.qe.model.Step;
+import com.redhat.qe.model.Volume;
+import com.redhat.qe.model.gluster.VolumeStatusOutput;
+import com.redhat.qe.model.jaxb.MigrateBrickAction;
+import com.redhat.qe.repository.JobRepository;
+import com.redhat.qe.repository.glustercli.VolumeXmlRepository;
+import com.redhat.qe.repository.rest.BrickRepository;
+import com.redhat.qe.repository.rest.ResponseWrapper;
+import com.redhat.qe.repository.rest.StepRepository;
+import com.redhat.qe.ssh.ExecSshSession;
+import com.redhat.qe.ssh.ExecSshSession.Response;
+import com.redhat.qe.test.rest.PopulatedVolumeTestBase;
+
+public class MigrateLastBrickTest extends MigrateTestBase{
+
+	@Override
+	protected Volume getVolumeToBeCreated() {
+		return VolumeFactory.distributed("startbriciikmigrate",4, getHost1(), getHost2());
+	}
+//	
+//	@Override
+//	public void cleanupRhsc(){
+//		//TODO don't clean up
+//	}
+//	
+//	@Override
+//	public void destroyVolume(){
+//		//TodO dont' do that
+//	}
+//	
+	
+	@Test
+	@Tcms({"318705"})
+	public void testRestStartedStatus(){
+		BrickRepository brickRepo = new BrickRepository(getSession(), volume.getCluster(), volume);
+		
+		ArrayList<Brick> bricks = brickRepo.list();
+		ResponseWrapper migrateAction = brickRepo._migrate(bricks.toArray(new Brick[0]));
+		migrateAction.expect("(?i)cannot remove all.*bricks");
+	}
+
+
+}
