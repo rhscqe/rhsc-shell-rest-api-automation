6dcac397f1b1a5d7508327f033901eb3ef52282f
Dustin Tsang <dtsang@redhat.com>
2013-12-10 09:41:33 -0500

    test to add brick to volume

---
+++ src/test/java/com/redhat/qe/test/rest/volume/AddBrickTest.java
@@ -0,0 +1,23 @@
+package com.redhat.qe.test.rest.volume;
+
+import org.junit.Test;
+
+import com.redhat.qe.factories.BrickFactory;
+import com.redhat.qe.factories.VolumeFactory;
+import com.redhat.qe.model.Volume;
+import com.redhat.qe.repository.rest.BrickRepository;
+import com.redhat.qe.test.rest.VolumeTestBase;
+
+public class AddBrickTest extends VolumeTestBase{
+
+	@Override
+	protected Volume getVolumeToBeCreated() {
+		return VolumeFactory.distributed("addBrickTest", host1, host2);
+	}
+
+	@Test
+	public void test(){
+		BrickRepository repo = new BrickRepository(getSession(), volume.getCluster(), volume);
+		repo.create(BrickFactory.brick(getHost2()));
+	}
+}
