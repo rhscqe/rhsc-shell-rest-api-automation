c8ccc7b52f70f4d187c5c4930f5bdf9d53793200
Dustin Tsang <dtsang@redhat.com>
2013-12-10 17:44:21 -0500

    test for starting and stoppig a volume

---
+++ src/test/java/com/redhat/qe/test/rest/volume/StartVolumeTest.java
@@ -0,0 +1,24 @@
+package com.redhat.qe.test.rest.volume;
+
+import org.junit.Test;
+
+import com.redhat.qe.factories.VolumeFactory;
+import com.redhat.qe.model.Volume;
+import com.redhat.qe.test.rest.VolumeTestBase;
+
+public class StartVolumeTest extends VolumeTestBase {
+
+	@Override
+	protected Volume getVolumeToBeCreated() {
+		return VolumeFactory.distributed("volumeToBeStarted", getHost1(), getHost2());
+	}
+	
+	@Test
+	public void test(){
+		getVolumeRepository().start(volume);
+		getVolumeRepository().show(volume);
+		getVolumeRepository()._stop(volume).expectSimilarCode(200);
+		getVolumeRepository().show(volume);
+	}
+
+}
