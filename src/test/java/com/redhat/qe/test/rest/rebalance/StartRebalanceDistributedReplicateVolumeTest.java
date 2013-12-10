e113820e3da75b8db99e778c096bad8bbb4ae9aa
Dustin Tsang <dtsang@redhat.com>
2013-12-10 11:39:56 -0500

    add more data in test fixture for StartRebalanceDistributedReplicateVolumeTest

--- src/test/java/com/redhat/qe/test/rest/rebalance/StartRebalanceDistributedReplicateVolumeTest.java
+++ src/test/java/com/redhat/qe/test/rest/rebalance/StartRebalanceDistributedReplicateVolumeTest.java
@@ -3,13 +3,31 @@ package com.redhat.qe.test.rest.rebalance;
 import org.junit.Test;
 
 import com.redhat.qe.annoations.Tcms;
+import com.redhat.qe.config.RhscConfiguration;
 import com.redhat.qe.factories.BrickFactory;
 import com.redhat.qe.factories.VolumeFactory;
+import com.redhat.qe.helpers.rebalance.BrickPopulator;
+import com.redhat.qe.helpers.utils.FileNameHelper;
+import com.redhat.qe.helpers.utils.FileSize;
 import com.redhat.qe.model.Action;
 import com.redhat.qe.model.Volume;
+import com.redhat.qe.repository.sh.DD;
+import com.redhat.qe.ssh.ExecSshSession;
 import com.redhat.qe.test.rest.RebalanceTestBase;
 
 public class StartRebalanceDistributedReplicateVolumeTest extends RebalanceTestBase{
+
+	protected void populateVolume() {
+		ExecSshSession mounterSession = ExecSshSession.fromHost( mounter);
+		mounterSession.start();
+		try{
+			for(int i=0; i < 26; i++){
+				mounterSession.runCommandAndAssertSuccess(DD.writeZeros(mountPoint.add(FileNameHelper.randomFileName()).toString(), FileSize.megaBytes(500)).toString());
+			}
+		}finally{
+			mounterSession.stop();
+		}
+	}
 
 	@Test
 	@Tcms("311346")
