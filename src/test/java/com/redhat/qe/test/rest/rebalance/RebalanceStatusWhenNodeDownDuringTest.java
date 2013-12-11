72319143bd7785ac5f5a7349b9d6062bd6256a14
Dustin Tsang <dtsang@redhat.com>
2013-12-11 05:46:53 -0500

    311413 rebalance when glusterd goes down during rebalance

--- src/test/java/com/redhat/qe/test/rest/rebalance/RebalanceStatusWhenNodeDownDuringTest.java
+++ src/test/java/com/redhat/qe/test/rest/rebalance/RebalanceStatusWhenNodeDownDuringTest.java
@@ -1,18 +1,12 @@
 package com.redhat.qe.test.rest.rebalance;
 
-import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import junit.framework.Assert;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import com.redhat.qe.annoations.Tcms;
 import com.redhat.qe.config.RhscConfiguration;
 import com.redhat.qe.factories.VolumeFactory;
-import com.redhat.qe.helpers.rebalance.BrickPopulator;
 import com.redhat.qe.helpers.repository.JobRepoHelper;
 import com.redhat.qe.helpers.repository.StepsRepositoryHelper;
 import com.redhat.qe.helpers.utils.AbsolutePath;
@@ -30,23 +24,11 @@ import com.redhat.qe.repository.rest.StepRepository;
 import com.redhat.qe.repository.sh.DD;
 import com.redhat.qe.ssh.ExecSshSession;
 import com.redhat.qe.test.rest.RebalanceTestBase;
+
+import dstywho.timeout.Timeout;
 public class RebalanceStatusWhenNodeDownDuringTest extends RebalanceTestBase{
 
-	private Job job = null;
 
-	/**
-	 * @return the job
-	 */
-	public synchronized Job getJob() {
-		return job;
-	}
-
-	/**
-	 * @param job the job to set
-	 */
-	public synchronized void setJob(Job job) {
-		this.job = job;
-	}
 
 	@Override
 	protected void populateVolume() {
@@ -54,8 +36,9 @@ public class RebalanceStatusWhenNodeDownDuringTest extends RebalanceTestBase{
 		ExecSshSession sshSession = ExecSshSession.fromHost(mounter);
 		sshSession.start();
 		try {
-			for(int i=0; i< 10 ; i ++){
-				sshSession.runCommandAndAssertSuccess(DD.writeRandomData(file.toString(),FileSize.megaBytes(100)).toString());
+			for(int i=0; i< 15; i ++){
+				sshSession.runCommandAndAssertSuccess(DD.writeZeros(file.toString(),FileSize.megaBytes(500)).toString());
+				sshSession.runCommandAndAssertSuccess(DD.writeZeros(file.toString(),FileSize.megaBytes(500)).toString());
 			}
 		} finally {
 			sshSession.stop();
@@ -63,56 +46,39 @@ public class RebalanceStatusWhenNodeDownDuringTest extends RebalanceTestBase{
 	}
 
 	@Test
-	@Ignore
+	@Tcms("311413")
 	public void test() throws InterruptedException{
-		Thread startRebalanceThread = new Thread(new Runnable() {
-			
-			public void run() {
-				Action rebalAction = getVolumeRepository(volume.getCluster()).rebalance(volume);
-				setJob(rebalAction.getJob());
-			}
-		});
-		Thread stopGlusterDThread = new Thread(new Runnable() {
-			
-			public void run() {
-				stopGlusterOnHost1();
-			}
-		});
+		Action rebalAction = getVolumeRepository(volume.getCluster()).rebalance(volume);
+		Job job = showJob(rebalAction.getJob());
+		Timeout.TIMEOUT_ONE_SECOND.sleep();
+		Timeout.TIMEOUT_ONE_SECOND.sleep();
+
+		stopGlusterOnHost1();
 		
-		startRebalanceThread.start();
-		Thread.sleep(1000);
-		stopGlusterDThread.start();
-		stopGlusterDThread.join();
-		startRebalanceThread.join();
 
 		try{
 			getGlusterdTaskSttus();
-			final Job job = showJob(getJob());
-			assertTrue(WaitUtil.waitUntil(new dstywho.functional.Predicate() {
-				@Override
-				public Boolean act() {
-					return showJob(job).getStatus().getState().equalsIgnoreCase("finished");
-				}
-			}, 20).isSuccessful());
-			
-			ArrayList<Step> steps = getStepRepository(job).list();
+			final Job _job = showJob(job);
+			Assert.assertTrue(new JobRepoHelper().waitUntilJobFinished(new JobRepository(getSession()), _job).isSuccessful());
 			
 			Step rebalanceStep = new StepsRepositoryHelper().getRebalanceStep(getStepRepository(job));
 			Assert.assertEquals("FINISHED", rebalanceStep.getStatus().getState());
 			Assert.assertEquals("FINISHED", getStepRepository(job).show(rebalanceStep.getParentStep()).getStatus().getState());
 		}finally{
 			startGlusterOnHost1();
+			if(getHostRepository().show(getHost1()).getState().contains("operational")){
+				getHostRepository().activate(getHost1());
+				WaitUtil.waitForHostStatus(getHostRepository(), getHost1(), "up", 20);
+			}
 		}
 	}
 
-	/**
-	 * 
-	 */
 	private String getGlusterdTaskSttus() {
 		ExecSshSession host2session = ExecSshSession.fromHost(RhscConfiguration.getConfiguredHostFromBrickHost(getSession(), getHost2()));
 		host2session.start();
 		try{
 			String blah = new VolumeXmlRepository(host2session).status(volume).getTasks().get(0).getStatusStr();
+			host2session.runCommandAndAssertSuccess(String.format("gluster vol rebalance %s status --xml", volume.getName()));
 			System.out.println("xxxxxxxxxxxxxxxxxx" + blah);
 			return blah;
 			
@@ -141,6 +107,7 @@ public class RebalanceStatusWhenNodeDownDuringTest extends RebalanceTestBase{
 			host1session.stop();
 		}
 	}
+
 
 	/**
 	 * @param job
