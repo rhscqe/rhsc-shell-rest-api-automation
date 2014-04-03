f0bbd61ba3be43e39adf6d78ce32045edd9c1963
Dustin Tsang <dtsang@redhat.com>
2014-04-03 12:56:13 -0700

    add logging to BrickDirectoryDestroyer

--- src/main/java/com/redhat/qe/helpers/repository/BrickDirectoryDestroyer.java
+++ src/main/java/com/redhat/qe/helpers/repository/BrickDirectoryDestroyer.java
@@ -2,6 +2,7 @@ package com.redhat.qe.helpers.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
@@ -18,6 +19,7 @@ import com.redhat.qe.ssh.ExecSshSession;
 import com.redhat.qe.ssh.ExecSshSession.Response;
 
 public class BrickDirectoryDestroyer {
+	private static final Logger LOG = Logger.getLogger(BrickDirectoryDestroyer.class.getName());
 
 	public void stopAndDestroyBrickDirectories(VolumeRepository repo, ArrayList<Host> hostsWithCredentials, Volume volume){
 		ArrayList<Brick> bricks = new BrickRepository(repo.getSession(), repo.getCluster() , volume).list();
@@ -26,15 +28,21 @@ public class BrickDirectoryDestroyer {
 	}
 
 	public void destroyBrickDirectories(VolumeRepository repo, List<Host> hosts, ArrayList<Brick> bricks) {
+		int i=0;
+		int max = bricks.size()-1;
+		LOG.info("deleting " + max + " brick dirs");
 		for(final Brick brick: bricks){
 			Host hostWithName = new com.redhat.qe.repository.rest.HostRepository(repo.getSession()).show(brick.getHost());
 			Host host = getHostByName(hosts, hostWithName);
+			LOG.fine("deleting brick dir " + i + "of " + max + " bricks:" + host.getName() + ";" +  brick.toString());  
 			ExecSshSession.fromHost(host).withSession(new Function<ExecSshSession, ExecSshSession.Response>() {
 					public Response apply(ExecSshSession session) {
 						return new DirectoryHelper().removeDirectory(session, new AbsolutePath(Path.from(brick.getDir())));
 					}
 			});
+			i++;
 		}
+		LOG.info("finished deleting " + max + " brick dirs");
 	}
 
 	private Host getHostByName(List<Host> hosts, final Host host) {
